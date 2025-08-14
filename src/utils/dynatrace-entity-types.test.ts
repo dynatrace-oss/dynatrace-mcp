9280813ba6c3bc28ca7071599865ff9f1969391d
Christian Kreuzberger <christian.kreuzberger@dynatrace.com>
2025-08-14 10:59:28 +0200

    chore: Refactor unit test into a table-test

--- src/utils/dynatrace-entity-types.test.ts
+++ src/utils/dynatrace-entity-types.test.ts
@@ -80,89 +80,97 @@ describe('getEntityTypeFromId', () => {
 
   // Tests for verified entity types found in the environment
   describe('Verified Entity Types', () => {
-    it('should map ENVIRONMENT entity ID to dt.entity.environment', () => {
-      const result = getEntityTypeFromId('ENVIRONMENT-2D0F07E264ABBB14');
-      expect(result).toBe('dt.entity.environment');
-    });
+    const verifiedEntityTypesTestCases = [
+      {
+        entityId: 'ENVIRONMENT-2D0F07E264ABBB14',
+        expectedType: 'dt.entity.environment',
+        description: 'ENVIRONMENT entity ID',
+      },
+      {
+        entityId: 'KUBERNETES_NODE-03169E17F0A60BE7',
+        expectedType: 'dt.entity.kubernetes_node',
+        description: 'KUBERNETES_NODE entity ID',
+      },
+      {
+        entityId: 'SERVICE_INSTANCE-02072B90BE476A9D',
+        expectedType: 'dt.entity.service_instance',
+        description: 'SERVICE_INSTANCE entity ID',
+      },
+      {
+        entityId: 'PROCESS_GROUP_INSTANCE-02072B90BE476A9D',
+        expectedType: 'dt.entity.process_group_instance',
+        description: 'PROCESS_GROUP_INSTANCE entity ID',
+      },
+      {
+        entityId: 'EC2_INSTANCE-4F09B64F7C5EC72D',
+        expectedType: 'dt.entity.ec2_instance',
+        description: 'EC2_INSTANCE entity ID',
+      },
+      {
+        entityId: 'AWS_LAMBDA_FUNCTION-65CB1C926C7A1C03',
+        expectedType: 'dt.entity.aws_lambda_function',
+        description: 'AWS_LAMBDA_FUNCTION entity ID',
+      },
+      {
+        entityId: 'CLOUD_APPLICATION_INSTANCE-00BAA09E6EC91E39',
+        expectedType: 'dt.entity.cloud_application_instance',
+        description: 'CLOUD_APPLICATION_INSTANCE entity ID',
+      },
+      {
+        entityId: 'DCG_INSTANCE-02072B90BE476A9D',
+        expectedType: 'dt.entity.docker_container_group_instance',
+        description: 'DCG_INSTANCE entity ID',
+      },
+      {
+        entityId: 'OS-006B30874F10C837',
+        expectedType: 'dt.entity.os',
+        description: 'OS entity ID',
+      },
+      {
+        entityId: 'AWS_AVAILABILITY_ZONE-0BAF92AC03BF3E25',
+        expectedType: 'dt.entity.aws_availability_zone',
+        description: 'AWS_AVAILABILITY_ZONE entity ID',
+      },
+      {
+        entityId: 'AWS_APPLICATION_LOAD_BALANCER-52D56F3FD851FF0C',
+        expectedType: 'dt.entity.aws_application_load_balancer',
+        description: 'AWS_APPLICATION_LOAD_BALANCER entity ID',
+      },
+      {
+        entityId: 'AWS_NETWORK_LOAD_BALANCER-617A06EFED8F4AB4',
+        expectedType: 'dt.entity.aws_network_load_balancer',
+        description: 'AWS_NETWORK_LOAD_BALANCER entity ID',
+      },
+      {
+        entityId: 'SYNTHETIC_LOCATION-0000000000000010',
+        expectedType: 'dt.entity.synthetic_location',
+        description: 'SYNTHETIC_LOCATION entity ID',
+      },
+      {
+        entityId: 'GEOLOCATION-0000000000000000',
+        expectedType: 'dt.entity.geolocation',
+        description: 'GEOLOCATION entity ID',
+      },
+      {
+        entityId: 'GCP_ZONE-0C3C7E567EAAB95B',
+        expectedType: 'dt.entity.gcp_zone',
+        description: 'GCP_ZONE entity ID',
+      },
+      {
+        entityId: 'AZURE_VM-42EA5AB8F028E280',
+        expectedType: 'dt.entity.azure_vm',
+        description: 'AZURE_VM entity ID',
+      },
+      {
+        entityId: 'RELATIONAL_DATABASE_SERVICE-B4EDC0E1E1279494',
+        expectedType: 'dt.entity.relational_database_service',
+        description: 'RELATIONAL_DATABASE_SERVICE entity ID',
+      },
+    ];
 
-    it('should map KUBERNETES_NODE entity ID to dt.entity.kubernetes_node', () => {
-      const result = getEntityTypeFromId('KUBERNETES_NODE-03169E17F0A60BE7');
-      expect(result).toBe('dt.entity.kubernetes_node');
-    });
-
-    it('should map SERVICE_INSTANCE entity ID to dt.entity.service_instance', () => {
-      const result = getEntityTypeFromId('SERVICE_INSTANCE-02072B90BE476A9D');
-      expect(result).toBe('dt.entity.service_instance');
-    });
-
-    it('should map PROCESS_GROUP_INSTANCE entity ID to dt.entity.process_group_instance', () => {
-      const result = getEntityTypeFromId('PROCESS_GROUP_INSTANCE-02072B90BE476A9D');
-      expect(result).toBe('dt.entity.process_group_instance');
-    });
-
-    it('should map EC2_INSTANCE entity ID to dt.entity.ec2_instance', () => {
-      const result = getEntityTypeFromId('EC2_INSTANCE-4F09B64F7C5EC72D');
-      expect(result).toBe('dt.entity.ec2_instance');
-    });
-
-    it('should map AWS_LAMBDA_FUNCTION entity ID to dt.entity.aws_lambda_function', () => {
-      const result = getEntityTypeFromId('AWS_LAMBDA_FUNCTION-65CB1C926C7A1C03');
-      expect(result).toBe('dt.entity.aws_lambda_function');
-    });
-
-    it('should map CLOUD_APPLICATION_INSTANCE entity ID to dt.entity.cloud_application_instance', () => {
-      const result = getEntityTypeFromId('CLOUD_APPLICATION_INSTANCE-00BAA09E6EC91E39');
-      expect(result).toBe('dt.entity.cloud_application_instance');
-    });
-
-    it('should map DCG_INSTANCE entity ID to dt.entity.docker_container_group_instance', () => {
-      const result = getEntityTypeFromId('DCG_INSTANCE-02072B90BE476A9D');
-      expect(result).toBe('dt.entity.docker_container_group_instance');
-    });
-
-    it('should map OS entity ID to dt.entity.os', () => {
-      const result = getEntityTypeFromId('OS-006B30874F10C837');
-      expect(result).toBe('dt.entity.os');
-    });
-
-    it('should map AWS_AVAILABILITY_ZONE entity ID to dt.entity.aws_availability_zone', () => {
-      const result = getEntityTypeFromId('AWS_AVAILABILITY_ZONE-0BAF92AC03BF3E25');
-      expect(result).toBe('dt.entity.aws_availability_zone');
-    });
-
-    it('should map AWS_APPLICATION_LOAD_BALANCER entity ID to dt.entity.aws_application_load_balancer', () => {
-      const result = getEntityTypeFromId('AWS_APPLICATION_LOAD_BALANCER-52D56F3FD851FF0C');
-      expect(result).toBe('dt.entity.aws_application_load_balancer');
-    });
-
-    it('should map AWS_NETWORK_LOAD_BALANCER entity ID to dt.entity.aws_network_load_balancer', () => {
-      const result = getEntityTypeFromId('AWS_NETWORK_LOAD_BALANCER-617A06EFED8F4AB4');
-      expect(result).toBe('dt.entity.aws_network_load_balancer');
-    });
-
-    it('should map SYNTHETIC_LOCATION entity ID to dt.entity.synthetic_location', () => {
-      const result = getEntityTypeFromId('SYNTHETIC_LOCATION-0000000000000010');
-      expect(result).toBe('dt.entity.synthetic_location');
-    });
-
-    it('should map GEOLOCATION entity ID to dt.entity.geolocation', () => {
-      const result = getEntityTypeFromId('GEOLOCATION-0000000000000000');
-      expect(result).toBe('dt.entity.geolocation');
-    });
-
-    it('should map GCP_ZONE entity ID to dt.entity.gcp_zone', () => {
-      const result = getEntityTypeFromId('GCP_ZONE-0C3C7E567EAAB95B');
-      expect(result).toBe('dt.entity.gcp_zone');
-    });
-
-    it('should map AZURE_VM entity ID to dt.entity.azure_vm', () => {
-      const result = getEntityTypeFromId('AZURE_VM-42EA5AB8F028E280');
-      expect(result).toBe('dt.entity.azure_vm');
-    });
-
-    it('should map RELATIONAL_DATABASE_SERVICE entity ID to dt.entity.relational_database_service', () => {
-      const result = getEntityTypeFromId('RELATIONAL_DATABASE_SERVICE-B4EDC0E1E1279494');
-      expect(result).toBe('dt.entity.relational_database_service');
+    it.each(verifiedEntityTypesTestCases)('should map $description to $expectedType', ({ entityId, expectedType }) => {
+      const result = getEntityTypeFromId(entityId);
+      expect(result).toBe(expectedType);
     });
   });
 });
